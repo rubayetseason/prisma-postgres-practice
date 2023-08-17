4ce33279020ed7b2aa039ee71001eb7564f906f1
rubayet-islam <season.ss2001@gmail.com>
2023-08-17 13:40:59 +0600

    prisma models created

--- prisma-practice/index.ts
+++ prisma-practice/index.ts
@@ -3,17 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const getAllUsers = await prisma.user.findMany();
-  console.log(getAllUsers);
 
-  //   const postUser = await prisma.user.create({
-  //     data: {
-  //       name: "Rubayet",
-  //       email: "rubayet@gmail.com",
-  //     },
-  //   });
-
-  //   console.log(postUser);
 }
 
 main();
